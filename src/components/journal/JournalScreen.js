b9e4f0b8ea01c637a289240c5240f093f7a4d200
Alex <wilmer.delgadoalama09@gmail.com>
2021-08-01 00:27:20 -0500

    first project

---
+++ src/components/journal/JournalScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { Sidebar } from './Sidebar';
+
+export const JournalScreen = () => {
+    return (
+        <div className="journal__main-content">
+            <Sidebar />
+            <main>
+                content!!
+            </main>
+        </div>
+    )
+}
